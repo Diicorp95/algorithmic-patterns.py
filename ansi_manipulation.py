8144165e0c8bcb113564f416fcb122cb01607267
Diicorp95 <>
2022-05-29 13:13:40 +0300

    Black format

--- ansi_manipulation.py
+++ ansi_manipulation.py
@@ -1,26 +1,30 @@
 # ANSI manipulations functions
 
+
 def horizontal_reflection(image):
-    result = ''
-    for line in image.split('\n'):
-    result += line[::-1] + '\n'
+    result = ""
+    for line in image.split("\n"):
+        result += line[::-1] + "\n"
     return result
+
 
 def vertical_reflection(image):
-    result = '\n'.join(image.split('\n')[::-1])
+    result = "\n".join(image.split("\n")[::-1])
     return result
 
-def add_horizontal_reflection(image, to_right_side = True):
-    result = ''
-    for line in image.split('\n'):
-    if to_right_side:
-        result += line + line[::-1]
-    else:
-        result += line[::-1] + line
-    result += '\n'
+
+def add_horizontal_reflection(image, to_right_side=True):
+    result = ""
+    for line in image.split("\n"):
+        if to_right_side:
+            result += line + line[::-1]
+        else:
+            result += line[::-1] + line
+        result += "\n"
     return result
 
-def add_vertical_reflection(image, to_bottom = True):
-    part = '\n'.join(image.split('\n')[::-1])
-    result = part + '\n' + image
+
+def add_vertical_reflection(image, to_bottom=True):
+    part = "\n".join(image.split("\n")[::-1])
+    result = part + "\n" + image
     return result
--- pixelgrid.py
+++ pixelgrid.py
@@ -1,39 +1,42 @@
 # ANSI output functions
 
+
 def boxshade(color):
-    palette = [
-        '\u20',
-        '\u2591',
-        '\u2592',
-        '\u2593',
-        '\u2588'
-    ]
+    palette = ["\u0020", "\u2591", "\u2592", "\u2593", "\u2588"]
     return palette[color % len(palette)]
 
-def plot(pixels, widenchars = False, size = 1, doPrint = True, useANSI = False):
+
+def plot(pixels, widenchars=False, size=1, doPrint=True, useANSI=False):
     mt = size
     if widenchars:
         mt *= 2
-    clear = lambda: (print('\033[0;0m', end = ''),
-        print('\033[2J', end = ''),
-        print('\033[0;0H', end = '')) if useANSI else None
+    clear = (
+        lambda: (
+            print("\033[0;0m", end=""),
+            print("\033[2J", end=""),
+            print("\033[0;0H", end=""),
+        )
+        if useANSI
+        else None
+    )
     clear()
     _f = []
-    for i, j in enumerate(pixels):
-        _ = ''
-        for i2, k in enumerate(pixels[i]):
-            _ += '{}'.format(boxshade(k) * mt)
+    for j in pixels:
+        _ = ""
+        for i2, k in enumerate(j):
+            _ += "{}".format(boxshade(k) * mt)
         _f.append(_)
-    y = ''
+    y = ""
     for i, j in enumerate(_f):
-        _ = ''
+        _ = ""
         for k in j:
             _ += k
-        else:
-            y += (_ + '\n') * size
+        y += (_ + "\n") * size
+    # else: ???
+    y = y[:-1]  # Remove the last new line character
     if doPrint:
         print(y)
     else:
         return y
-    input('\n')
+    input("")
     clear()
